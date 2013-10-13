35f123a3b5f0e734c78d8823864f5faf8c4afa6d
kathryn <keb97@cornell.edu>
2013-10-13 16:42:55 -0700

    final version of tests

--- spec/features/api_call_spec.rb
+++ spec/features/api_call_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "API Call" do
   it "calls API and response used in index" do
-  	VCR.use_cassette("hotels", :allow_playback_repeats => true) do
+  	VCR.use_cassette("hotels") do
   	  visit '/' 
   	  page.should have_content "Hotel"
   	  page.should_not have_content "NotHotel"
--- spec/features/checkbox_sorting_spec.rb
+++ spec/features/checkbox_sorting_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature "User checkboxes to sort deals" do
-  VCR.use_cassette("hotels", :record => :new_episodes) do
+  VCR.use_cassette("hotels") do
 
     before do
   	    visit '/'
@@ -10,15 +10,21 @@ feature "User checkboxes to sort deals" do
     scenario "Select and unselect only Great Value", :js => true do
   	  page.find(:css, '[name=GreatValue]').click
   	  page.should_not have_css('div.low-val')
+  	  page.should_not have_content("30.0%")
+
   	  page.find(:css, '[name=GreatValue]').click
   	  page.should have_css('div.low-val')
+  	  page.should have_content("30.0%")
     end
 
     scenario "Select and unselect only Top Rated", :js => true do
   	  page.find(:css, '[name=TopRated]').click
   	  page.should_not have_css('div.low-star')
+  	  page.should_not have_content("2.5/4.0")
+  	  
   	  page.find(:css, '[name=TopRated]').click
   	  page.should have_css('div.low-star')
+  	  page.should have_content("2.5/4.0")
     end
 
     scenario "Select and unselect both Great Value and Top Rated", :js => true do
@@ -32,7 +38,6 @@ feature "User checkboxes to sort deals" do
   	  page.should have_css('div.low-val')
   	  page.should have_css('div.low-star')
     end
-
   end
 end
 

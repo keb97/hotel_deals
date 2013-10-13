a001fbd824e35bf2700d65c03a2fa5ff27258e2e
kathryn <keb97@cornell.edu>
2013-10-12 20:01:13 -0700

    removing possibly extraneous js

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -4,8 +4,4 @@ class DealsController < ApplicationController
   	@response = HTTParty.get('http://deals.expedia.com/beta/deals/hotels.json')
   end
 
-  def search
-  	@response = @response.select {|i| i["totalRate"] < 30}
-  end	
-
 end

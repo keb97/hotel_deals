0d646acf7e297d5e9ceeb4bdd1c3421b064c97b0
kathryn <keb97@cornell.edu>
2013-10-13 00:32:40 -0700

    buttons under modal work

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,8 @@
 class DealsController < ApplicationController
 
   def index
-  	@response = HTTParty.get('http://deals.expedia.com/beta/deals/hotels.json')
+  	response = HTTParty.get('http://deals.expedia.com/beta/deals/hotels.json')
+    @response = response.sort_by { |d| -d["rawAppealScore"]}
   end
 
 end

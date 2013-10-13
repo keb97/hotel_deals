5aba4445886769c6f4839c672b673609e736b124
kathryn <keb97@cornell.edu>
2013-10-12 19:40:48 -0700

    try to push before shutdown

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -2,12 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(function(){
-  $("#chooseLanguage").change(function(){
-    if ($(this).val() == '') {
-      $("#uploadFile").hide();
-    } else {
-      $("#uploadFile").show();
-    }
-  });
-});
